a6df3eb7b08541bf92cd52bd305033e07728e059
Alexis Arando <lex4736@gmail.com>
2021-04-11 17:33:14 -0700

    installing dependencies and setup folder file structure

---
+++ model/bookdata.js
@@ -0,0 +1,16 @@
+  
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Book = mongoose.model("Book", bookData);
+
+const bookData = new Schema({
+  title: { type: String, required: true },
+  subtitle: { type: String },
+  authors: { type: [String], required: true },
+  link: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  googleId: { type: String, required: true, unique: true }
+});
+
+module.exports = Book;
